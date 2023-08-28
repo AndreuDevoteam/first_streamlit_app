f6e8a692760ada45282922f64db2941f84d8464b
AndreuDevoteam <143392804+AndreuDevoteam@users.noreply.github.com>
2023-08-28 12:15:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 streamlit.title('La nueva comida sana de mis padres')
 streamlit.header('Desayuno')
 streamlit.text('🥣 Leche con cereales')
@@ -6,3 +8,6 @@ streamlit.text(' 🥑🍞Pan con sobrasada')
 streamlit.text('🥗 🐔Lechuga cual Gallina')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
