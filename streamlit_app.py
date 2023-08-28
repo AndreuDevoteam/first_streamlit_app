db7a00dde5d0dd5ff13ae6a67bf196ec847d611d
AndreuDevoteam <143392804+AndreuDevoteam@users.noreply.github.com>
2023-08-28 12:06:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('La nueva comida sana de mis padres')
 streamlit.header('Desayuno')
-streamlit.text('Leche con cereales')
-streamlit.text('Pan con sobrasada')
-streamlit.text('Lechuga cual vaca')
+streamlit.text('🥣 Leche con cereales')
+streamlit.text(' 🥑🍞Pan con sobrasada')
+streamlit.text('🥗 🐔Lechuga cual Gallina')
